d4c0f4fa97f8ab62c77e229d0b81f7103cd660d9
max <hardbitec0c0c0@gmail.com>
2022-10-08 20:07:35 +0300

    coomit message

--- src/redux/contactsSlicer.js
+++ src/redux/contactsSlicer.js
@@ -1,30 +1,48 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { nanoid } from 'nanoid';
+import { fetchContacts } from './operations';
 
 const contactsSlice = createSlice({
   name: 'contacts',
-  initialState: [],
-
-  reducers: {
-    addContacts: {
-      reducer(state, action) {
-        console.log(state.contacts);
-        state.push(action.payload);
-      },
-      prepare({ name, number }) {
-        return {
-          payload: {
-            id: nanoid(10),
-            name,
-            number,
-          },
-        };
-      },
+  initialState: {
+    contacts: [],
+    isLoading: false,
+    error: null,
+  },
+  extraReducers: {
+    [fetchContacts.pending](state) {
+      state.isLoading = true;
     },
-    deleteContacts(state, action) {
-      return state.filter(contact => contact.id !== action.payload);
+    [fetchContacts.fulfilled](state, action) {
+      state.isLoading = false;
+      state.error = null;
+      state.items = action.payload;
+    },
+    [fetchContacts.rejected](state, action) {
+      state.isLoading = false;
+      state.error = action.payload;
     },
   },
+  // extraReducers: {
+  //   addContacts: {
+  //     reducer(state, action) {
+  //       console.log(state.contacts);
+  //       state.push(action.payload);
+  //     },
+  //     prepare({ name, number }) {
+  //       return {
+  //         payload: {
+  //           id: nanoid(10),
+  //           name,
+  //           number,
+  //         },
+  //       };
+  //     },
+  //   },
+  //   deleteContacts(state, action) {
+  //     return state.filter(contact => contact.id !== action.payload);
+  //   },
+  // },
 });
 
 export const { addContacts, deleteContacts } = contactsSlice.actions;
---
+++ src/redux/operations.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+axios.defaults.baseURL = 'https://633f431c0dbc3309f3c7c597.mockapi.io';
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addContacts = createAsyncThunk(
+  'contacts/addContacts',
+  async (text, thunkAPI) => {
+    try {
+      const response = await axios.post('/contacts', { text });
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteContacts = createAsyncThunk(
+  'contacts/deleteContacts',
+  async (contactsId, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/contacts/${contactsId}`);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
